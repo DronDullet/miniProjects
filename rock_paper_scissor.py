651f440ebc02d0e4b3e7d0947b5e743e38a4d72f
DronDullet <steperkov#yandex.ru>
2023-11-04 22:07:39 +0300

    Написал мини игру "камень ножницы бумага". Не тратил время на рефакторинг

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -3,44 +3,51 @@ import random
 
 choise_list = ["камень", "ножницы", "бумага"]
 
-computer = random.choice(choise_list)
-player = input('Сделай выбор из "камень", "ножницы", "бумага": ').lower()
-while player not in choise_list:
-    print("Этого в списке нет, сделай правильный выбор!")
+
+while True:
+    computer = random.choice(choise_list)
     player = input('Сделай выбор из "камень", "ножницы", "бумага": ').lower()
-    print(player)
+    while player not in choise_list:
+        print("Этого в списке нет, сделай правильный выбор!")
+        player = input(
+            'Сделай выбор из "камень", "ножницы", "бумага": ').lower()
+        print(player)
 
+    if player == computer:
+        print("Компьютер выбрал:", computer)
+        print("Ты выбрал :", player)
+        print("Ничья!")
+    elif player == "камень":
+        if computer == "ножницы":
+            print("Компьютер выбрал:", computer)
+            print("Ты выбрал :", player)
+            print("Вы выиграли!")
+        elif computer == "бумага":
+            print("Компьютер выбрал:", computer)
+            print("Ты выбрал :", player)
+            print("Вы проиграли!")
 
-if player == computer:
-    print("Компьютер выбрал:", computer)
-    print("Ты выбрал :", player)
-    print("Ничья!")
-elif player == "камень":
-    if computer == "ножницы":
-        print("Компьютер выбрал:", computer)
-        print("Ты выбрал :", player)
-        print("Вы выиграли!")
-    elif computer == "бумага":
-        print("Компьютер выбрал:", computer)
-        print("Ты выбрал :", player)
-        print("Вы проиграли!")
+    elif player == "ножницы":
+        if computer == "бумага":
+            print("Компьютер выбрал:", computer)
+            print("Ты выбрал :", player)
+            print("Вы выиграли!")
+        elif computer == "камень":
+            print("Компьютер выбрал:", computer)
+            print("Ты выбрал :", player)
+            print("Вы проиграли!")
 
-elif player == "ножницы":
-    if computer == "бумага":
-        print("Компьютер выбрал:", computer)
-        print("Ты выбрал :", player)
-        print("Вы выиграли!")
-    elif computer == "камень":
-        print("Компьютер выбрал:", computer)
-        print("Ты выбрал :", player)
-        print("Вы проиграли!")
+    elif player == "бумага":
+        if computer == "камень":
+            print("Компьютер выбрал:", computer)
+            print("Ты выбрал :", player)
+            print("Вы выиграли!")
+        elif computer == "ножницы":
+            print("Компьютер выбрал:", computer)
+            print("Ты выбрал :", player)
+            print("Вы проиграли!")
 
-elif player == "бумага":
-    if computer == "камень":
-        print("Компьютер выбрал:", computer)
-        print("Ты выбрал :", player)
-        print("Вы выиграли!")
-    elif computer == "ножницы":
-        print("Компьютер выбрал:", computer)
-        print("Ты выбрал :", player)
-        print("Вы проиграли!")
+    game = input("Сыграем еще разок? (да/нет):")
+    if game != "да":
+        print("Игра окончена!")
+        break
