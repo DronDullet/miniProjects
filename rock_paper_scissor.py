a78e90e2b19c166fb2d72b7013e3f7959dce35c6
DronDullet <steperkov#yandex.ru>
2023-11-04 21:41:24 +0300

    Игра работает, но пока что одним циклом

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -10,5 +10,37 @@ while player not in choise_list:
     player = input('Сделай выбор из "камень", "ножницы", "бумага": ').lower()
     print(player)
 
-print("Компьютер выбрал:", computer)
-print("Ты выбрал :", player)
+
+if player == computer:
+    print("Компьютер выбрал:", computer)
+    print("Ты выбрал :", player)
+    print("Ничья!")
+elif player == "камень":
+    if computer == "ножницы":
+        print("Компьютер выбрал:", computer)
+        print("Ты выбрал :", player)
+        print("Вы выиграли!")
+    elif computer == "бумага":
+        print("Компьютер выбрал:", computer)
+        print("Ты выбрал :", player)
+        print("Вы проиграли!")
+
+elif player == "ножницы":
+    if computer == "бумага":
+        print("Компьютер выбрал:", computer)
+        print("Ты выбрал :", player)
+        print("Вы выиграли!")
+    elif computer == "камень":
+        print("Компьютер выбрал:", computer)
+        print("Ты выбрал :", player)
+        print("Вы проиграли!")
+
+elif player == "бумага":
+    if computer == "камень":
+        print("Компьютер выбрал:", computer)
+        print("Ты выбрал :", player)
+        print("Вы выиграли!")
+    elif computer == "ножницы":
+        print("Компьютер выбрал:", computer)
+        print("Ты выбрал :", player)
+        print("Вы проиграли!")
